067a1deca9b75dd32d384757229698841162cb38
yjnl <114989228+yjnl@users.noreply.github.com>
2022-12-15 18:49:24 +0000

    regular red

--- Circles/WebContent/scripts/ex8.js
+++ Circles/WebContent/scripts/ex8.js
@@ -1,5 +1,5 @@
 function d3Draw() {
-	var dataset = [ 5, 10, 15, 20, 25, 50 ];
+	var dataset = [ 5, 10, 15, 20, 25, 30 ];
 	Width = 300;
 	Height = 200;
 	var svg = d3.select("body").append("svg").attr("width", Width).attr(
